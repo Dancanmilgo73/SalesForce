ac9b84d8a0d8334fcfcbc53fec0776d724b5c6d2
Dancanmilgo73 <dancanmilgo73@gmail.com>
2022-01-06 15:39:20 +0300

    adding the  dotenv

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var jsforce = require("jsforce");
-const username = "dancan@tech.com";
-const password = "Ghasiawewe2022C8ZM7jcnHASRqW3P13w5MoCNw";
-var conn = new jsforce.Connection({
+require.env.config();
+const jsforce = require("jsforce");
+
+const conn = new jsforce.Connection({
 	// you can change loginUrl to connect to sandbox or prerelease env.
 	// loginUrl: "https://test.salesforce.com",
 	// securityToken: "C8ZM7jcnHASRqW3P13w5MoCNw",
@@ -10,19 +10,23 @@ https://techcom-5a-dev-ed.my.salesforce.com
 User ID: 0058d000001113kAAA
 Org ID: 00D8d0000020VV2EAM */
 });
-conn.login(username, password, function (err, userInfo) {
-	if (err) {
-		return console.error(err.message);
+conn.login(
+	process.env.username,
+	process.env.password,
+	function (err, userInfo) {
+		if (err) {
+			return console.error(err.message);
+		}
+		// Now you can get the access token and instance URL information.
+		// Save them to establish connection next time.
+		console.log(conn.accessToken);
+		console.log(conn.instanceUrl);
+		// logged in user property
+		console.log("User ID: " + userInfo.id);
+		console.log("Org ID: " + userInfo.organizationId);
+		// ...
 	}
-	// Now you can get the access token and instance URL information.
-	// Save them to establish connection next time.
-	console.log(conn.accessToken);
-	console.log(conn.instanceUrl);
-	// logged in user property
-	console.log("User ID: " + userInfo.id);
-	console.log("Org ID: " + userInfo.organizationId);
-	// ...
-});
+);
 // var records = [];
 // conn.query("SELECT Id, Name FROM Account", function (err, result) {
 // 	if (err) {
